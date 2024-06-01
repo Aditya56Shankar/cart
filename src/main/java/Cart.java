a76d6568573dff82704c2bc7cbcde4baa2624512
Aditya56Shankar <122800149+Aditya56Shankar@users.noreply.github.com>
2024-06-02 01:47:54 +0530

    Update Cart.java

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Cart {
+import java.util.Scanner;
+
+public class Cart {
+
+    static float r1 = 20.0f;
+    static float r2 = 20.0f;
+    static float r3 = 20.0f;
+
+    public static void main(String[] args) {
+        float a = 0.2f;
+        float b = 0.3f;
+        float c = 0.5f;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Requirement: ");
+        int req = in.nextInt();
+
+        float a1 = req * a;
+        float a2 = req * b;
+        float a3 = req * c;
+
+        if (r1>=a1 && r2>=a2 && r3>=a3){
+            System.out.println("data is sufficient");
+            System.out.println("req of r1 is "+a1);
+            System.out.println("req of r2 is "+a2);
+            System.out.println("req of r3 is "+a3);
+        } else{
+            System.out.println("data is insufficient");
+        }
+    }
 }
+
